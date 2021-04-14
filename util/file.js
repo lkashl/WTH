bba5e42fbf7426d3849403bdf94d83074b4af7a0
Kash <lkashl@hotmail.com>
2021-04-15 00:12:59 +1000

    - Added fs util polyfill for older node versions

--- util/file.js
+++ util/file.js
@@ -1,11 +1,28 @@
-const fs = require('fs/promises');
+// Polyfill for older node versions that do not contain fs/promises module
 
-const open = fs.open;
-const readFile = fs.readFile;
-const readdir = fs.readdir;
-const fstat = fs.stat;
-const mkdir = fs.mkdir;
-const appendFile = fs.appendFile;
+let open, readFile, readdir, fstat, mkdir, appendFile;
+
+try {
+    const fs = require('fs/promises');
+
+    open = fs.open;
+    readFile = fs.readFile;
+    readdir = fs.readdir;
+    fstat = fs.stat;
+    mkdir = fs.mkdir;
+    appendFile = fs.appendFile;
+} catch (err) {
+    const fs = require('fs');
+    const { promisify } = require('util');
+
+    open = promisify(fs.open);
+    readFile = promisify(fs.readFile)
+    readdir = promisify(fs.readdir)
+    fstat = promisify(fs.stat)
+    mkdir = promisify(fs.mkdir)
+    appendFile = promisify(fs.appendFile)
+}
+
 
 module.exports = {
     open, readFile, readdir, fstat, mkdir, appendFile
